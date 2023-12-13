e9a708202007c1bc8ebf9c73066d9fe9bc18c1fe
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-13 15:36:06 -0500

    Skeleton Hitbox fix

--- Lost-Souls/src/entities/Skeleton.js
+++ Lost-Souls/src/entities/Skeleton.js
@@ -4,7 +4,7 @@ import StateMachine from "../../lib/StateMachine.js";
 import Direction from "../enums/Direction.js";
 import EnemyStateName from "../enums/EnemyStateName.js";
 import ImageName from "../enums/ImageName.js";
-import { images } from "../globals.js";
+import { DEBUG, images, context} from "../globals.js";
 import SkeletonFallingState from "../states/Skeleton/SkeletonFallingState.js";
 import SkeletonIdleState from "../states/Skeleton/SkeletonIdleState.js";
 import Enemy from "./Enemy.js";
@@ -36,7 +36,7 @@ export default class Skeleton extends Enemy{
         this.gravityForce = new Vector(0, 1000);
 
         this.direction = Direction.Left;
-        this.hitboxOffsets = new Hitbox(Skeleton.WIDTH-4, Skeleton.HEIGHT-8, -Skeleton.OFFSET_WIDTH + Skeleton.WIDTH, -Skeleton.OFFSET_HEIGHT+Skeleton.HEIGHT);
+        this.hitboxOffsets = new Hitbox(Skeleton.WIDTH+8, Skeleton.HEIGHT-8, -Skeleton.OFFSET_WIDTH + Skeleton.WIDTH, -Skeleton.OFFSET_HEIGHT+Skeleton.HEIGHT);
 
         this.idleSprites = Sprite.generateSpritesFromSpriteSheet(
             images.get(ImageName.SkeletonIdle),
@@ -60,6 +60,14 @@ export default class Skeleton extends Enemy{
         this.stateMachine.add(EnemyStateName.Idle, new SkeletonIdleState(this));
         this.stateMachine.add(EnemyStateName.Falling, new SkeletonFallingState(this));
         this.stateMachine.change(EnemyStateName.Idle);
+    }
+    render(){
+        
+        context.save();
+
+        super.render(this.positionOffset);
+        
+        context.restore();
     }
 
     moveDown(dt){
