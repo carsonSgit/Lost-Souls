34aa516cae6abd03833ce34280fe20b575fae661
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-12 20:59:33 -0500

    Slide animation Hotfix

--- Lost-Souls/src/states/Player/PlayerRollingState.js
+++ Lost-Souls/src/states/Player/PlayerRollingState.js
@@ -11,7 +11,7 @@ export default class PlayerRollingState extends State{
         this.player = player;
 
 
-        this.animation = new Animation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0.1 , 1);
+        this.animation = new Animation([0, 1, 2, 3, 4, 6, 7, 8, 9], 0.1 , 1);
         console.log(this.animation);
         this.originalHitboxOffsets = this.player.hitboxOffsets;
     }
